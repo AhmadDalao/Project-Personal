9cf2cd36df9db4d84668be3924021ee9e5ef6ef8
Ahmad Dalao <shadow8evil@gmail.com>
2023-06-02 23:32:50 +0300

    finished 2 sections

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,12 @@
-console.log("working");
+
+
+let mega_menu_trigger = document.querySelector("#mega_menu_trigger");
+let mega_menu = document.querySelector(".mega-menu");
+
+mega_menu_trigger.addEventListener("click", triggerMegaMenu);
+
+
+function triggerMegaMenu() {
+    mega_menu_trigger.classList.toggle("menu-trigger");
+    mega_menu.classList.toggle("show-menu");
+}
